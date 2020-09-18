c68dae6948c3f3c52a8dadb33d514c4c59e90feb
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-09-18 13:53:46 +0200

    First Trial
    Very First Version of the application. You can play the folding animation by pressing the next or previous buttons.
    - installed TMPro
    - imported Animation Prefabs
    - created script for "next" and "previous" button
    - exported for standalone and android

---
+++ 3D Origami/Assets/Scripts/TutorialController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TutorialController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] folds = new GameObject[9];
+
+    private int _index;
+
+    void Awake()
+    {
+        folds[0].SetActive(true);
+        _index = 0;
+    }
+    
+    public void NextAnimation()
+    {
+        if (_index != 8)
+        {
+            folds[_index].SetActive(false);
+            folds[_index + 1].SetActive(true);
+            _index += 1;
+        }
+    }
+    
+    public void PreviousAnimation()
+    {
+        if (_index != 0)
+        {
+            folds[_index].SetActive(false);
+            folds[_index - 1].SetActive(true);
+            _index -= 1;
+        }
+    }
+}
