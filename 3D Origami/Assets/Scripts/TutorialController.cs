9a8617ea594df8ed4579de6c834925fad1ae1169
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-01 15:54:40 +0200

    new animations and new texture
    - texture makes edges more visible
    - different colors for different sections of the paper piece
    - colors generataed with colors.co, accessible for different levels of color blindness

--- 3D Origami/Assets/Scripts/TutorialController.cs
+++ 3D Origami/Assets/Scripts/TutorialController.cs
@@ -69,5 +69,26 @@ public class TutorialController : MonoBehaviour
         pauseButton.text = "Pause";
         _paused = false;
     }
+    
+  /*  Animator m_Animator;
+    //Value from the slider, and it converts to speed level
+    float m_MySliderValue;
+
+    void Start()
+    {
+        //Get the animator, attached to the GameObject you are intending to animate.
+        m_Animator = folds[_index].GetComponent<Animator>();
+    }
+
+    void OnGUI()
+    {
+        //Create a Label in Game view for the Slider
+        GUI.Label(new Rect(0, 25, 40, 60), "Speed");
+        //Create a horizontal Slider to control the speed of the Animator. Drag the slider to 1 for normal speed.
+
+        m_MySliderValue = GUI.HorizontalSlider(new Rect(45, 25, 200, 800), m_MySliderValue, 0.0F, 1.0F);
+        //Make the speed of the Animator match the Slider value
+        m_Animator.speed = m_MySliderValue;
+    }*/
 
 }
