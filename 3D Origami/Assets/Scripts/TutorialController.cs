a66a9210d154e1aabc71ba673e59a1054c8d5492
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-05 17:13:42 +0200

    new UI layout with icons
    - replaced standard Buttons with Icons from flaticon.com

--- 3D Origami/Assets/Scripts/TutorialController.cs
+++ 3D Origami/Assets/Scripts/TutorialController.cs
@@ -1,12 +1,19 @@
-﻿using TMPro;
+﻿
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
     [SerializeField]
     private GameObject[] folds = new GameObject[7];
     [SerializeField]
-    private TextMeshProUGUI pauseButton;
+    private Button pauseButton;
+    [SerializeField]
+    private Sprite Unpause;
+    [SerializeField]
+    private Sprite Pause;
+
+    private string[] instructions; 
     private int _index;
     private bool _paused;
     void Awake()
@@ -15,8 +22,17 @@ public class TutorialController : MonoBehaviour
         _index = 0;
         _paused = true; // if it is set as true in the beginning, the PauseAnimation() method flips it to false
         PauseOrUnpauseAnimation();
+        instructions = new[] 
+            {"1. Fold the paper in half horizontally.", 
+                "2. Fold it vertically and open it up again to get a vertical crease.", 
+                "3. Fold both sides towards that crease.", 
+                "4. Turn the paper on its back.",
+                "5. Fold the small corners inwards.",
+                "6. Fold up the two protruding flaps.",
+                "7. Fold it in half and you are done!"
+            };
     }
-    
+
     public void NextAnimation()
     {
         if (_index != folds.Length - 1)
@@ -59,18 +75,18 @@ public class TutorialController : MonoBehaviour
     private void PauseAnimation()
     {
         folds[_index].GetComponent<Animator>().speed = 0f; //pauseAnimation
-        pauseButton.text = "Unpause";
+        pauseButton.image.overrideSprite = Unpause;//"Unpause";
         _paused = true;
     }
     
     private void UnpauseAnimation()
     {
         folds[_index].GetComponent<Animator>().speed = 1f; //unpauseAnimation
-        pauseButton.text = "Pause";
+        pauseButton.image.overrideSprite = Pause; //"Pause";
         _paused = false;
     }
     
-  /*  Animator m_Animator;
+    /* Animator m_Animator;
     //Value from the slider, and it converts to speed level
     float m_MySliderValue;
 
