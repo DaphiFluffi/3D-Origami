0ec67b73bde63c753f1dab8785d80ca0161abf96
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-26 19:18:52 +0100

    cylinder generator is working
    - CircleGenerator.cs has GenerateCylinder method

--- Assets/Scripts/3_Part_Generator/CircleGenerator.cs
+++ Assets/Scripts/3_Part_Generator/CircleGenerator.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class CircleGenerator : MonoBehaviour
 {
-    public GameObject prefabToInstantiate;
+    [SerializeField] private GameObject prefabToInstantiate = default;
+    [SerializeField] private float rows = 4f;
+    [SerializeField] private int amountPerRow = 5;
+    [SerializeField] private float radius = 1f;
+    [SerializeField] private float distance= 1f;
   // https://answers.unity.com/questions/1068513/place-8-objects-around-a-target-gameobject.html
     /// <summary>
     ///     Instantiates prefabs around center splited equality. 
@@ -19,33 +23,59 @@ public class CircleGenerator : MonoBehaviour
     ///     The margin from center, if your center is at (1,1,1) and your radius is 3 
     ///     your final position can be (4,1,1) for example
     /// </param>
-    /// <param name="yPosition">The yPostion for the instantiated prefabs</param>
-    private void InstantiateInCircle(GameObject prefab, Vector3 location, int howMany, float radius, float yPosition)
+    
+    private void InstantiateInCircle(GameObject prefab, Vector3 location, int howMany, float radius)
     {
         float angleSection = Mathf.PI * 2f / howMany;
         for (int i = 0; i < howMany; i++)
         {
             float angle = i * angleSection;
             Vector3 newPos = location + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
-            newPos.y = yPosition;
+           // newPos.y = yPosition;
             //https://forum.unity.com/threads/instantiate-prefab-towards-object.134213/
             Instantiate(prefab, newPos, Quaternion.LookRotation (location - newPos)); // so that the pieces look towards the center 
         }
     }
 
-    private void InstantiateInCircle(GameObject prefab, Vector3 location, int howMany, float radius)
+    private void GenerateCylinder(float rows, GameObject prefab, int amountPerRow, float radius, float distance)
     {
-        this.InstantiateInCircle(prefab, location, howMany, radius, location.y);
+        Vector3 center = new Vector3(0, 0, 0);
+        float angleSection = Mathf.PI * 2f / amountPerRow;
+        float angle = 0f;
+        GameObject cylinder = new GameObject {name = "cylinder"};
+        for (int r = 0; r < rows; r++){
+            center.y = distance * r;
+            GameObject parentObject = new GameObject(); //create an 'empty' object
+            parentObject.name = r+1 + ".row" ;  //set it's name
+            parentObject.transform.parent = cylinder.transform;
+            for (int a = 0; a < amountPerRow; a++)
+            {
+                //print("row: " + r + " amount: " + a);
+                if (r % 2 == 0) // even row starts counting at 0 degrees
+                {
+                    angle = a * angleSection;
+                }
+                else // odd row start counting at half of the angleSection
+                {
+                    angle = (a * angleSection) + (angleSection / 2f);
+                }
+
+                Vector3 spawnPosition = center + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+                GameObject piece = Instantiate(prefab, spawnPosition, Quaternion.LookRotation (center - spawnPosition)); // so that the pieces look towards the center 
+                piece.name = a+1 + ".piece in the " + (r+1) + ".row";
+                piece.transform.parent = parentObject.transform;
+            }
+        }
     }
     public void InstantiateInCircle(GameObject prefab, int howMany, float radius)
     {
-        this.InstantiateInCircle(prefab, this.transform.position, howMany, radius);
+        this.InstantiateInCircle(prefab, this.transform.position, howMany, radius); //location is location of the Empty Game Object
     }
     // client EXAMPLE
     private void Start()
     {
-        this.InstantiateInCircle(this.prefabToInstantiate, new Vector3(0,0,0), 12, 2);
-        this.InstantiateInCircle(this.prefabToInstantiate, new Vector3(0,1.5f,0), 12, 2);
-
-    } //ohne die y = 2 position
+       // this.InstantiateInCircle(this.prefabToInstantiate, new Vector3(0,0,0), 12, 2); //ohne die y = 2 position
+        // this.InstantiateInCircle(this.prefabToInstantiate, new Vector3(0,1.5f,0), 12, 2);
+        GenerateCylinder(rows, prefabToInstantiate, amountPerRow, radius, distance);
+    } 
 }
