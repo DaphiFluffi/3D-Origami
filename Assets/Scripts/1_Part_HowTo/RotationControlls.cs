c29a374cdb771735b59f33b28fee194b485f8142
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-26 20:01:22 +0100

    documented generator code

--- Assets/Scripts/1_Part_HowTo/RotationControlls.cs
+++ Assets/Scripts/1_Part_HowTo/RotationControlls.cs
@@ -13,14 +13,14 @@ public class RotationControlls : MonoBehaviour
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
         float rotationX = Input.GetAxis("Vertical") * rotationSpeed;
-        //float rotationY = Input.GetAxis("Vertical") * rotationSpeed;
-        float rotationZ = Input.GetAxis("Horizontal") * rotationSpeed;
+        float rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
+       //float rotationZ = Input.GetAxis("Horizontal") * rotationSpeed;
         
         rotationX *= Time.deltaTime;
-        //rotationY *= Time.deltaTime;
-        rotationZ *= Time.deltaTime;
+        rotationY *= Time.deltaTime;
+        //rotationZ *= Time.deltaTime;
 
         // Rotate around our y-axis
-        transform.Rotate(rotationX,0 , rotationZ);
+        transform.Rotate(rotationX, rotationY , 0);
     }
 }
