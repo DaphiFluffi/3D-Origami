df08df297396cbf9f6e4d42b310867ee1a03f580
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-30 23:11:30 +0100

    multiple inverted Rows can now be generated

--- Assets/Scripts/3_Part_Generator/CircleGenerator.cs
+++ Assets/Scripts/3_Part_Generator/CircleGenerator.cs
@@ -1,24 +1,24 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 public class CircleGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject pieceModel = default;
-   /* [SerializeField] private float rows = 4f;
+    /*[SerializeField] private float rows = 4f;
     [SerializeField] private int amountPerRow = 5;
     [SerializeField] private bool collapsed = false;
     [SerializeField] private int invertedRow = default;*/
 
     private GameObject generatedCylinder;
     private bool isCreated;
+    GameObject piece;
     /*private void Start()
     {
         GenerateCylinder(rows, prefabToInstantiate, amountPerRow, invertedRow);
     }*/
 
-    //wenn das inverted Row array leer ist 
-    
     /// <summary> 
     ///    Spawns a cylinder with an equal amount of pieces per row
     ///    while the amount of rows is up to the user.
@@ -30,20 +30,19 @@ public class CircleGenerator : MonoBehaviour
     ///     The margin from center, if your center is at (1,1,1) and your radius is 3 
     ///     your final position can be (4,1,1) for example </param>
     /// <param name="distance">Distance between rows</param>
-    public void GenerateCylinder(float howManyRows, int amountPerRow, int invertedRow, bool collapsed)
+    
+    public void GenerateCylinder(float howManyRows, int amountPerRow, List<int> invertedRows, bool collapsed)
     {
         Destroy(generatedCylinder);
         isCreated = false;
-        if (!isCreated)
+        if (!isCreated) // so that only one cylinder is created
         {
             Vector3 center = new Vector3(-4, 0, 0);
-            float radius, angle;
+         
+            float angle;
             float angleSection = Mathf.PI * 2f / amountPerRow;
             float distance = 1.5f; // determined empirically
-            if (collapsed)
-            {
-                distance = 0.5f;
-            }
+            if (collapsed) { distance = 0.5f; }
 
             // parent cylinder object
             generatedCylinder = new GameObject {name = "cylinder"};
@@ -67,33 +66,59 @@ public class CircleGenerator : MonoBehaviour
                         angle = (a * angleSection) + (angleSection / 2f);
                     }
 
-                    if (r == invertedRow - 1)
+                    if(invertedRows.Contains(0))
                     {
-                        radius = 0.13f * amountPerRow; // was determined empirically
+                        piece = AssemblePieces(angle, 0.08f, amountPerRow, center);
                     }
                     else
                     {
-                        radius = 0.08f * amountPerRow; // 0.08f was determined empirically
+                        if (invertedRows.Contains(r + 1))
+                        {
+                            piece = AssemblePieces(angle, 0.13f, amountPerRow, center);
+                            piece.transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
+                        }
+                        else
+                        {
+                            piece = AssemblePieces(angle, 0.08f, amountPerRow, center);
+                        }
                     }
-
-                    // How to instantiate game objects around a point https://answers.unity.com/questions/1068513/place-8-objects-around-a-target-gameobject.html
-                    // How to make game objects look towards a point https://forum.unity.com/threads/instantiate-prefab-towards-object.134213/
-                    Vector3 spawnPosition = center + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
-                    // so that the pieces look towards the center 
-                    GameObject piece = Instantiate(pieceModel, spawnPosition, Quaternion.LookRotation(center - spawnPosition));
-
-                    if (r == invertedRow - 1)
-                    {
-                        piece.transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
-                    }
-
+                    
                     // naming every instantiated piece according to its respective row 
                     piece.name = a + 1 + ".piece in the " + (r + 1) + ".row";
                     // parenting every piece to its respective row 
                     piece.transform.parent = row.transform;
+                    
+                    /*GameObject piece;
+                    
+                    if (r == invertedRow - 1)
+                    {
+                        piece = AssemblePieces(angle, 0.13f, amountPerRow, center);
+                        piece.transform.rotation *= Quaternion.Euler(0f, 180f, 0f);
+                        // row.name += "inverted"; //adds the inverted suffix as ofter as we have pieces per inverted row 
+                    }
+                    else
+                    {
+                        piece = AssemblePieces(angle, 0.08f, amountPerRow, center);
+                    }
+                    // naming every instantiated piece according to its respective row 
+                    piece.name = a + 1 + ".piece in the " + (r + 1) + ".row";
+                    // parenting every piece to its respective row 
+                    piece.transform.parent = row.transform;*/
                 }
             }
             isCreated = true;
         }
     }
+
+    private GameObject AssemblePieces(float angle, float radius, int amountPerRow, Vector3 center)
+    {
+        radius = radius * amountPerRow; 
+        // How to instantiate game objects around a point https://answers.unity.com/questions/1068513/place-8-objects-around-a-target-gameobject.html
+        // How to make game objects look towards a point https://forum.unity.com/threads/instantiate-prefab-towards-object.134213/
+        Vector3 spawnPosition = center + new Vector3(Mathf.Cos(angle), 0, Mathf.Sin(angle)) * radius;
+        // so that the pieces look towards the center 
+        GameObject piece = Instantiate(pieceModel, spawnPosition, Quaternion.LookRotation(center - spawnPosition));
+        return piece;
+    }
+    
 }
