227cb4ac1e98a9bf78994b483ba7508cf113f162
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-10-31 20:39:57 +0100

    changing color of piece by clicking on it is possible
    - used cube prefab
    - changed mesh to exported mesh
    - changed default box collider to mesh collider
    - OrigamiColor.cs Script is added onto every piece by default

---
+++ Assets/Scripts/3_Part_Generator/ColorOrigami.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorOrigami : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                this.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+
+    /*void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                currentColor = (currentColor+1)%length;
+                this.GetComponent<MeshRenderer>().material.color = colors [currentColor];
+            }
+        }
+    }*/
+}
