5e9d52d11df7c39451b096b29f2fb2920c232891
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-11-02 20:49:16 +0100

    integrated color picker into UI and changes Rotation Script for Generator

--- Assets/Scripts/1_Part_HowTo/RotationControlls.cs
+++ Assets/Scripts/1_Part_HowTo/RotationControlls.cs
@@ -13,14 +13,12 @@ public class RotationControlls : MonoBehaviour
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
         float rotationX = Input.GetAxis("Vertical") * rotationSpeed;
-        float rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
-       //float rotationZ = Input.GetAxis("Horizontal") * rotationSpeed;
+        float rotationZ = Input.GetAxis("Horizontal") * rotationSpeed;
         
         rotationX *= Time.deltaTime;
-        rotationY *= Time.deltaTime;
-        //rotationZ *= Time.deltaTime;
-
-        // Rotate around our y-axis
-        transform.Rotate(rotationX, rotationY , 0);
+        rotationZ *= Time.deltaTime;
+        
+        // Rotate around new center at x = -4
+        transform.RotateAround(new Vector3(0,0.5f,0), new Vector3(rotationX,0, rotationZ), rotationSpeed * Time.deltaTime);
     }
 }
