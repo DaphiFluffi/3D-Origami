fd459dcefe72e9154986052222c49b3732d997b8
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-11-04 19:12:45 +0100

    redesigned UI and introduced Menu
    - changed WebGL Build Settings

---
+++ Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+   public void OnPart1Click()
+   {
+      SceneManager.LoadScene(1);
+   }
+
+   public void OnPart3Click()
+   {
+      SceneManager.LoadScene(2);
+   }
+
+   public void BackToMenu()
+   {
+      SceneManager.LoadScene(0);
+   }
+
+   public void Quit()
+   {
+      Application.Quit();
+   }
+}
