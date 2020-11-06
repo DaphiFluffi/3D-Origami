27bd5ae8eee4c0c63f496bcc5ff06fe137b0d314
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-11-06 13:56:26 +0100

    refactoring done, introduced camera pan

--- Assets/Scripts/3_Part_Generator/CalculateWidthHeight.cs
+++ Assets/Scripts/3_Part_Generator/CalculateWidthHeight.cs
@@ -6,38 +6,33 @@ public class CalculateWidthHeight : MonoBehaviour
 {
     private float width;
     private float height;
-    private bool calculated;
-    public void CalculateDimensions(float howManyRows, int amountPerRow, List<int> invertedRows)
+
+    public void CalculateDimensions(int[] rows, int amountPerRow)
     {
-        /*
-        Breite:
-        3,4cm + (0,05 cm * TeileProReihe)
-        */
         width = 3.4f + (0.05f * amountPerRow);
         /* 
-        Höhe:
-        normalen Reihen  = 2cm (Teil-Höhe) + (0,5 cm (1/4 der Teil-Höhe) * Menge an Reihen)
+        Höhe:normalen Reihen  = 2cm (Teil-Höhe) + (0,5 cm (1/4 der Teil-Höhe) * Menge an Reihen)
         1. Reihe eine inverted Row oder zwei aufeinanderforlgende : - 0,5 cm 
             normale Reihe - inverted Reihe - normale Reihe: +0,5 cm 
         */
-        height = 2f + 0.5f * (howManyRows - 1); //for normal rows 
-        if (invertedRows.Contains(1))
+        height = 2f + 0.5f * (rows.Length - 1); //for normal rows 
+
+        if (rows[0] == 1 && rows.Length > 1) //if we have an inverted row in the first row and it is not the only row
         {
             height -= 0.5f;
         }
-        // if we have invertedRows at all 
-        else if (!invertedRows.Contains(0))
+        for (int i = 0; i < rows.Length; i++)
         {
-            // for every inverted row like that add 0.5 cm unless we are in the top row
-            foreach (int inverted in invertedRows)
+            if (rows[i] == 1 && i > 0 && i < rows.Length - 1) //whenever we have an inverted row in the middle, but not at the top or bottom 
             {
-                if (inverted != ((int) howManyRows))
-                {
-                    height += 0.5f;
-                }
+                height += 0.5f;
             }
+          /*  else if (rows[i] == 1 && rows[i + 1] == 1 && (i+1) != rows.Length - 1)
+            {
+                height -= 0.5f;
+            }*/
         }
-        //Debug.Log("Width: " + width + " cm Height: " + height + " cm");
+        //Debug.Log("Width: " + width + " cm Height: " + height + " cm" + rows.Length);
     }
 
     public float GetWidth()
