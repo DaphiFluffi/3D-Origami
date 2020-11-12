283bd424bef446a8fbef2056d2dd84fb50cda47b
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-11-12 22:03:13 +0100

    worked out how to count colors using events

---
+++ Assets/Scripts/3_Part_Generator/ColorManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorManager : MonoBehaviour
+{
+    private int counter;
+   public  void Ping(Color colorWeGot)
+    {
+        counter++;
+        Debug.Log(counter);
+        Debug.Log(colorWeGot);
+    }
+}
--- Assets/Scripts/3_Part_Generator/ColorOrigami.cs
+++ Assets/Scripts/3_Part_Generator/ColorOrigami.cs
@@ -6,9 +6,17 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using Debug = UnityEngine.Debug;
+using UnityEngine.Events;
+
+//https://docs.unity3d.com/ScriptReference/Events.UnityEvent_1.html
+[System.Serializable]
+public class ColorEvent : UnityEvent<Color>
+{
+}
 
 public class ColorOrigami : MonoBehaviour
 {
+    public ColorEvent ColorChanged;
     private FlexibleColorPicker fcp;
     private Color currentColor;
     private Canvas canv;
@@ -16,18 +24,25 @@ public class ColorOrigami : MonoBehaviour
     private GameObject palettePanel;
     private Image paintPot;
     private TMP_Text amount;
-    
+    private ColorManager manager;
     void Awake()
     {
+        manager = FindObjectOfType<ColorManager>();
         canv = FindObjectOfType<Canvas>();
         fcp = canv.GetComponentInChildren<FlexibleColorPicker>(true);
         palettePanel = GameObject.FindGameObjectWithTag("Palette");
         paintPot = palettePanel.transform.GetChild(0).GetComponent<Image>();
+        
     }
 
     void Start()
     {
+        if (ColorChanged == null)
+            ColorChanged = new ColorEvent();
+
+        ColorChanged.AddListener(manager.Ping);
         currentColor = GetComponent<Renderer>().material.color;
+        
     }
     
     //TODO Quelle
@@ -42,14 +57,18 @@ public class ColorOrigami : MonoBehaviour
                 if (fcp != null && currentColor != fcp.color)
                 { 
                     GetComponent<Renderer>().material.color= fcp.color;
-                    Debug.Log("colorChanged");
+                    //Debug.Log("colorChanged");
                     currentColor = fcp.color;
+                    colorCounter++;
+                    // TODO Send out "colorChanged" event to ColorManager ListenerScript
+                    //Debug.Log(colorCounter);
+                    ColorChanged.Invoke(fcp.color);
                 }
             }
         }
     }
 
-
+    
 
     /*void Update()
     {
