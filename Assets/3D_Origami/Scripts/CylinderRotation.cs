c38d5224e3435719a524c19db20a23381a41b33c
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-12-06 18:16:56 +0100

    better icons

--- Assets/3D_Origami/Scripts/CylinderRotation.cs
+++ Assets/3D_Origami/Scripts/CylinderRotation.cs
@@ -10,8 +10,8 @@ public class CylinderRotation : MonoBehaviour
     private float startingPositionX;
     private float startingPositionY;
     private Button resetButton;
-    float rotationX = 0; 
-    float rotationY = 0;
+    private float rotationX, rotationY, GUIrotatationX, GUIrotatationY = 0;
+    
     private void ResetRotation()
     {
         transform.rotation = Quaternion.identity;
@@ -29,36 +29,32 @@ public class CylinderRotation : MonoBehaviour
         // The value is in the range -1 to 1
         // TODO restricted vertical rotation
         
-        if (SystemInfo.deviceType == DeviceType.Desktop)
-        {
-            rotationX = Input.GetAxis("Vertical") * rotationSpeed;
-            rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
-            //rotationX *= Time.deltaTime;
-            //rotationY *= Time.deltaTime;
-           // transform.RotateAround(new Vector3(0,0,0), new Vector3(rotationX, rotationY,0), rotationSpeed * Time.deltaTime);
-        }
-        if (SystemInfo.deviceType == DeviceType.Handheld)
-        {
-            //https://www.youtube.com/watch?v=XFbNTeW1d_8
-            rotationX = CrossPlatformInputManager.GetAxis("Vertical") * rotationSpeed;
-            rotationY = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
-        }
+        // rotation works with WSAD and Arrow Keys
+        rotationX = Input.GetAxis("Vertical") * rotationSpeed;
+        rotationY = Input.GetAxis("Horizontal") * rotationSpeed;
+
+        //https://www.youtube.com/watch?v=XFbNTeW1d_8
+        // rotation with on Screen keys
+        GUIrotatationX = CrossPlatformInputManager.GetAxis("Vertical") * rotationSpeed;
+        GUIrotatationY = CrossPlatformInputManager.GetAxis("Horizontal") * rotationSpeed;
         
         rotationX *= Time.deltaTime;
         rotationY *= Time.deltaTime;
-        
+        GUIrotatationX *= Time.deltaTime;
+        GUIrotatationY *= Time.deltaTime;
+
         //Generator Scene
         if (SceneManager.GetActiveScene().buildIndex == 2)
         {
-           // transform.RotateAround(new Vector3(0, 0, 0), new Vector3(rotationX, rotationY, 0),
-           //     rotationSpeed * Time.deltaTime);
             transform.Rotate(rotationX, rotationY, 0, Space.Self);
-
+            transform.Rotate(GUIrotatationX, GUIrotatationY, 0, Space.Self);
         }
+        
         // How to fold Pieces Scene
         else if (SceneManager.GetActiveScene().buildIndex == 1)
         {
             transform.Rotate(rotationX,0, rotationY, Space.Self);
+            transform.Rotate(GUIrotatationX,0, GUIrotatationY, Space.Self);
         }
         
         // reset rotation with space
@@ -66,42 +62,5 @@ public class CylinderRotation : MonoBehaviour
         {
             ResetRotation();
         }
-        
-        // Touch Drag Rotation
-        // thanks to https://answers.unity.com/questions/1681603/how-to-rotate-an-object-using-touch-controls.html
-       /* if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            switch (touch.phase)
-            {
-                case TouchPhase.Began:
-                    startingPositionX = touch.position.x;
-                    //startingPositionY = touch.position.y;
-                    break;
-                case TouchPhase.Moved:
-                    if (startingPositionX > touch.position.x)
-                    {
-                        transform.Rotate(new Vector3(0,1,0), rotationSpeed * Time.deltaTime);
-                    }
-                    else if (startingPositionX < touch.position.x)
-                    {
-                        transform.Rotate(new Vector3(0,1,0), -rotationSpeed * Time.deltaTime);
-                    }
-                    if (startingPositionY > touch.position.y)
-                    {
-                        transform.Rotate(new Vector3(1,0,0), -rotationSpeed * Time.deltaTime);
-                    }
-                    else if (startingPositionY < touch.position.y)
-                    {
-                        transform.Rotate(new Vector3(1,0,0), rotationSpeed * Time.deltaTime);
-                    }
-                    break;
-                case TouchPhase.Ended:
-                    Debug.Log("Touch Phase Ended.");
-                    break;
-            }
-        }*/
     }
-
-   
 }
