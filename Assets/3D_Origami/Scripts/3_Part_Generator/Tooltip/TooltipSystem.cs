3bd2bda5e40c8f08d7a3b8156f4f9f9452a0773c
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2020-12-13 15:56:05 +0100

    video player, tooltips, reset rotation on collapse

---
+++ Assets/3D_Origami/Scripts/3_Part_Generator/Tooltip/TooltipSystem.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://www.youtube.com/watch?v=HXFoUGw7eKk
+public class TooltipSystem : MonoBehaviour
+{
+    // Singleton reference 
+    private static TooltipSystem current;
+
+    [SerializeField] private Tooltip tooltip = default;
+    void Awake()
+    {
+        current = this;
+    }
+
+    public static void Show(string content)
+    {
+        current.tooltip.SetText(content);
+        current.tooltip.gameObject.SetActive(true);
+    }
+
+    public static void Hide()
+    {
+        current.tooltip.gameObject.SetActive(false);
+    }
+}
---
+++ Assets/3D_Origami/Scripts/3_Part_Generator/Tooltip/TooltipTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+//https://www.youtube.com/watch?v=HXFoUGw7eKk
+public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [Multiline]
+    [SerializeField] private string content = default; 
+    
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        StartCoroutine(Delay(2f));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        StopCoroutine(Delay(2f));
+        TooltipSystem.Hide();
+    }
+
+    IEnumerator Delay(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        TooltipSystem.Show(content);
+    }
+}
