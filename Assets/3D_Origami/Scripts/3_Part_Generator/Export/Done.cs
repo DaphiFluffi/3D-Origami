cbff0bfb3e747bdff69197dd677da94638fd033b
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2021-01-02 21:29:34 +0100

    fixed webGL export bug

--- Assets/3D_Origami/Scripts/3_Part_Generator/Export/Done.cs
+++ Assets/3D_Origami/Scripts/3_Part_Generator/Export/Done.cs
@@ -36,17 +36,18 @@ public class Done : MonoBehaviour
             return;
         }*/
 
-       string content = FBXExporter.MeshToString(objectToExport, null, true, true);
 
         #if UNITY_EDITOR
-            string path = EditorUtility.SaveFilePanel($"Export {objectToExport} as .fbx", "", objectToExport.name + ".fbx", "fbx");
+            string path = EditorUtility.SaveFilePanel($"Export {objectToExport} as .fbx", "", objectToExport.name.ToString() + ".fbx", "fbx");
             FBXExporter.ExportGameObjToFBX(objectToExport, path, true, true);
         #elif UNITY_WEBGL
+            string content = FBXExporter.MeshToString(objectToExport, null, true, true);
             WebGLFileSaver.SaveFile(content, "cylinder.fbx", "application/octet-stream");
         #elif UNITY_STANDALONE || UNITY_ANDROID	
         // https://forum.unity.com/threads/simple-file-browser-open-source.441908/page-7#post-6642685
            FileBrowser.ShowSaveDialog( ( paths ) =>
            {
+               string content = FBXExporter.MeshToString(objectToExport, null, true, true);
                string targetPath = paths[0];
                FileBrowserHelpers.WriteTextToFile(targetPath, content);
            }, null, FileBrowser.PickMode.Files, false, "C:\\", "3D_Origami_Model_", "Save As", "Save" );
