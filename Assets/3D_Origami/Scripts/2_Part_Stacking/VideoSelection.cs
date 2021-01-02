fc1b5cf9cefebe951335de9a75d967db16f4cd96
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2021-01-02 21:29:34 +0100

    fixed webGL export bug

--- Assets/3D_Origami/Scripts/2_Part_Stacking/VideoSelection.cs
+++ Assets/3D_Origami/Scripts/2_Part_Stacking/VideoSelection.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using TMPro;
-using UnityEditor.UIElements;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
