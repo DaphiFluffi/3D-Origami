5a2a1efff9091dbcfe8f1612188fd82a5ed572fd
DaphiFluffi <42743003+DaphiFluffi@users.noreply.github.com>
2021-01-19 14:46:34 +0100

    imported new animation exports that rotate as expected

--- Assets/3D_Origami/Scripts/CylinderRotation.cs
+++ Assets/3D_Origami/Scripts/CylinderRotation.cs
@@ -42,19 +42,20 @@ public class CylinderRotation : MonoBehaviour
         GUIrotatationX *= Time.deltaTime;
         GUIrotatationY *= Time.deltaTime;
 
-     /*   switch (currentSceneIndex)
+        switch (currentSceneIndex)
         {
             // How to fold Pieces Scene
+            //negative y
             case 1:
-                transform.Rotate(rotationX,0, rotationY, Space.Self);
-                transform.Rotate(GUIrotatationX,0, GUIrotatationY, Space.Self);
+                transform.Rotate(rotationX,-1 * rotationY,0, Space.Self);
+                transform.Rotate(GUIrotatationX, -1 * GUIrotatationY, 0, Space.Self);
                 break;
             //Generator Scene
-            case 3:*/
+            case 3:
                 transform.Rotate(rotationX, rotationY, 0, Space.Self);
                 transform.Rotate(GUIrotatationX, GUIrotatationY, 0, Space.Self);
-              /*  break;
-        }*/
+                break;
+        }
         
         // reset rotation with space
         if(Input.GetKeyDown(KeyCode.Return))
